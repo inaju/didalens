b8ae1017e28dc15b7cc7a58c3281f8c9740dd491
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-08 12:38:00 +0100

    These are the working files

--- dida/settings.py
+++ dida/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from .secrets import *
+from secret_folder.secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
