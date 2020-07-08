2fa8965a4d4b910e52dbc476abe6724c94f9321a
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-08 23:12:28 +0100

    trying again

--- dida/settings.py
+++ dida/settings.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url 
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['AMASON_PASSWORD'], os.environ['AMAZON_USER'],  os.environ['DATABASE_URL'], os.environ['KEY:'] )
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -18,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(KEY)
+SECRET_KEY = os.getenv(KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -135,8 +138,8 @@ LOGOUT_REDIRECT_URL='home'
 EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =os.environ.get(AMAZON_USER)
-EMAIL_HOST_PASSWORD =os.environ.get(AMASON_PASSWORD)
+EMAIL_HOST_USER =os.getenv(AMAZON_USER)
+EMAIL_HOST_PASSWORD =os.getenv(AMASON_PASSWORD)
 
 
 # Activate Django-Heroku.
