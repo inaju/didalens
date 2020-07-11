cad18584e9d794bfc26de5f50accf0ec4c5a9b01
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-11 18:56:12 +0100

    trying out a different function

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -11,15 +11,15 @@ import sys
 def job():
     try:
         #webbrowser.open('http://didalens.herokuapp.com/goals/goalfull/')
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-        driver.get('https://duckduckgo.com')
-        driver.get('http://didalens.herokuapp.com/goals/goalfull/')
+        options = Options()
 
+        options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+
+        return webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=options)
         print('done')
     except:
         e = sys.exc_info()[0]
