3c95b7d25baec944bca775717d694b27ea90172d
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 10:57:21 +0100

    editing somefiles

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -18,7 +18,7 @@ def job():
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--remote-debugging-port=9222')
-        driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
+        driver = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
         driver.get("didalens.herokuapp.com/goals/fakeemail/")
         driver.close()
         
