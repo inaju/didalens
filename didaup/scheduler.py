48926363f826852666b80350329b6560dd529991
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 10:53:07 +0100

    editing somefiles

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -14,11 +14,11 @@ def job():
         print(os.environ.get('CHROMEDRIVER_PATH'))
 
         chrome_options = Options()
-        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+        chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--remote-debugging-port=9222')
-        driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
         driver.get("didalens.herokuapp.com/goals/fakeemail/")
         driver.close()
         
