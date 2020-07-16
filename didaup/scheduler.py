a305ad0e503366faf7ccc9bcdb7d1ab4e508434b
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 11:59:32 +0100

    editing somefiles

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -18,7 +18,7 @@ def job():
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome(executable_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
 
         # Now you can start using Selenium    except:
     except:
