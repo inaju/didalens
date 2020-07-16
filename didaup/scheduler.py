6c4f33507eedf8393c9c591aedeeaa715fe3ed1d
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 10:06:47 +0100

    reset the build packs

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -18,7 +18,7 @@ def job():
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--remote-debugging-port=9222')
-        chrome_options.binary_location = os.environ.get('CHROMEDRIVER_PATH')
+        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
         driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
         driver.get("didalens.herokuapp.com/goals/fakeemail/")
         
