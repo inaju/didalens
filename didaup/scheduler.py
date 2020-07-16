c4715ceda06263be1c7b5e57eb09398461a71889
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 13:24:25 +0100

    editing somefiles

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -16,12 +16,6 @@ def job():
         chrome_options = webdriver.ChromeOptions()
         
         
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu") 
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--ignore-certificate-errors')
-        chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN_S')
         driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH_S'), options=chrome_options)
 
