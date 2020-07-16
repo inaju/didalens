a79dd3775bec8294be55cdfe2f28b9067e4cd8e2
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 12:30:36 +0100

    editing somefiles

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -14,11 +14,16 @@ def job():
         import os
 
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN_S')
-        chrome_options.add_argument("--no-sandbox")
+        
+        
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
+        chrome_options.add_argument("--disable-gpu") 
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN_S')
+        driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH_S'), chrome_options=chrome_options)
 
         # Now you can start using Selenium    except:
     except:
