636b7bf5984995a91b4622eeb03ab725a81cf627
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 09:53:31 +0100

    changed binary file location

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -23,21 +23,10 @@ def job():
         driver.get("didalens.herokuapp.com/goals/fakeemail/")
         
         print('done')
+        
     except:
         e = sys.exc_info()
         print('it failed, this is the error ', e)
-        driver.close()
-
-        chrome_options = Options()
-        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--remote-debugging-port=9222')
-        driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
-        driver.get("didalens.herokuapp.com/goals/fakeemail/")
-        driver.close()
-        print('done in except')
 
 
 schedule.every(0.01).minutes.do(job)
