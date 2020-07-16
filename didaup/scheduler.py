2d7720344e804b266283ebf7add2878cc45e232f
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-16 08:32:30 +0100

    editing somefiles

--- didaup/scheduler.py
+++ didaup/scheduler.py
@@ -19,10 +19,17 @@ def job():
         chrome_options.add_argument('--no-sandbox')
         driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
         driver.get("didalens.herokuapp.com/goals/fakeemail/")
+        driver.close()
         print('done')
-    except:
-        e = sys.exc_info()[0]
-        print('it failed, this is the error ', e)
+    except selenium.common.exceptions.WebDriverException:
+        chrome_options = Options()
+        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+        driver.get("didalens.herokuapp.com/goals/fakeemail/")
+        driver.close()
+        print('done')
 
 
 schedule.every(0.01).minutes.do(job)
