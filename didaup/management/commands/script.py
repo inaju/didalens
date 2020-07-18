450b1dec10360294faef922900eb95b27143b5f4
mitchel inaju <mitchelinajuo@gmail.com>
2020-07-18 13:07:27 +0100

    using appscheduler module for cron job

--- didaup/management/commands/script.py
+++ didaup/management/commands/script.py
@@ -11,25 +11,64 @@ import os
 import requests
 import webbrowser
 import sys
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+sched = BlockingScheduler()
 
-class Command(BaseCommand):
-    help = 'Scrapes spoj.com to obtain the details of all the classical problems.'
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=23)
+def scheduled_job():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+
+    print('This job is run by 11pm UTC and 12am WAT.')
+    class Command(BaseCommand):
+        help = 'Scrapes spoj.com to obtain the details of all the classical problems.'
     
-    def handle(self, *args, **options):
-        print('pretty')
+        def handle(self, *args, **options):
+            print('pretty')
 
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu") 
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--ignore-certificate-errors')
-        chrome_options.add_argument("--remote-debugging-port=9222")
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu") 
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--ignore-certificate-errors')
+            chrome_options.add_argument("--remote-debugging-port=9222")
 
-        chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN_S')
-       
-        driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH_S'), options=chrome_options)
-        driver.get('https://didalens.herokuapp.com/goals/goalreminder/')
+            chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN_S')
         
-        print('it worked')
+            driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH_S'), options=chrome_options)
+            driver.get('https://didalens.herokuapp.com/goals/goalreminder/')
+            
+            print('it worked')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1)
+def email_job():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+
+    class Command(BaseCommand):
+        help = 'Scrapes spoj.com to obtain the details of all the classical problems.'
+
+        def handle(self, *args, **options):
+            print('pretty')
+
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu") 
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--ignore-certificate-errors')
+            chrome_options.add_argument("--remote-debugging-port=9222")
+
+            chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN_S')
+        
+            driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH_S'), options=chrome_options)
+            driver.get('https://didalens.herokuapp.com/goals/fakeemail/')
+            
+            print('it worked')
+
+
+
+sched.start()
