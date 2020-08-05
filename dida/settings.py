a43c01fc726733859b78a8ace37ff5a1650eac50
mitchelinaju <mitchelinajuo@gmail.com>
2020-08-06 00:49:09 +0100

    worked on procfile

--- dida/settings.py
+++ dida/settings.py
@@ -12,9 +12,10 @@ import dj_database_url
 from boto.s3.connection import S3Connection
 
 
-s3 = S3Connection(os.environ['SENDGRID_PASSWORD'], os.environ['DATABASE_URL'], os.environ['KEY'] )
+s3 = S3Connection('SENDGRID_PASSWORD','KEY')
 SECRET_KEY = os.getenv('KEY')
-EMAIL_HOST_USER = 'apikey'  
+EMAIL_HOST_USER = 'apikey' 
+EMAIL_HOST_PASSWORD =SENDGRID_PASSWORD
 SECRET_KEY = os.getenv('KEY')
 
 
